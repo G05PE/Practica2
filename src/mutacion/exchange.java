231ee5cd29d4ae241a7f71394a6b61a810464c57
Sergio <sramos02@ucm.es>
2020-03-11 13:08:25 +0100

    Exchange done

--- src/mutacion/exchange.java
+++ src/mutacion/exchange.java
@@ -1,18 +1,23 @@
 package mutacion;
 
+import java.util.Random;
+
+import poblacion.individuo;
 import poblacion.poblacion;
 
 public class exchange extends mutacion{
 
-	public exchange(String name) {
-		super(name);
-		// TODO Auto-generated constructor stub
-	}
-
 	@Override
 	public poblacion mutar(poblacion poblacion, double probMutacion) {
-		// TODO Auto-generated method stub
-		return null;
+		Random rand = new Random();
+		int var1 = rand.nextInt()%poblacion.getSize();
+		int var2 = rand.nextInt()%poblacion.getSize();
+		
+		individuo aux = poblacion.getIndividuo(var1);
+		poblacion.setIndividuoAt(var1, poblacion.getIndividuo(var1));
+		poblacion.setIndividuoAt(var2, aux);
+		
+		return poblacion;
 	}
 
 }
--- src/mutacion/mutacion.java
+++ src/mutacion/mutacion.java
@@ -4,16 +4,34 @@ import poblacion.poblacion;
 
 public abstract class mutacion {
 	private String name;
+	
 	public mutacion(String name) {
 		this.name=name;
 	}
+	
+	public mutacion() {}
+	
   public abstract poblacion mutar(poblacion poblacion, double probMutacion);
 
   public mutacion getCopia() {
 		switch(name) {
-		case "":
-			return null;//retornar una copia nueva
+		case "exchange":
+			return new exchange();
+			/*break;
+		case "heuristic":
+			return new heuristic();
+			break;
+		case "insercion":
+			return new insercion();
+			break;
+		case "investment":
+			return new investment();
+			break;
+		case "propio":
+			return new metPropio();
+			break;*/
 		}
 		return null;
 	}
+  
 }
