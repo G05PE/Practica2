43df72a4c241df2a0c8371b6c3a77f31c3479f5f
Sergio <sramos02@ucm.es>
2020-03-14 11:53:15 +0100

    la que estoy liando

--- src/cruces/pmx.java
+++ src/cruces/pmx.java
@@ -1,60 +1,13 @@
 package cruces;
 
-import java.util.Random;
-
-import genetica.gen;
-import poblacion.individuo;
 import poblacion.poblacion;
 
 public class pmx extends algoritmoCruce {
 
 	@Override
 	public poblacion cruzar(poblacion seleccionados, double prob) {
-		ini(prob, seleccionados);
-		seleccionaReproductores();	
-		cruzaReproductores();
-		
-		return getDescendientes();
+		// TODO Auto-generated method stub
+		return null;
 	}
 
-	//Elige un tramo de uno de los reproductores y cruza preservando el orden y la posición
-	private void cruzaReproductores() {
-		//Elige aleatoriamente dos puntos de corte
-		Random rand = new Random();
-		int var1 = rand.nextInt()%getReproductoresSize();
-		int var2 = rand.nextInt()%getReproductoresSize();
-		
-		//Fuerza que los puntos sean diferentes
-		while(var1 == var2)var2 = rand.nextInt()%getReproductoresSize();
-		
-		//Hacemos los puntos positivos y los ordenamos (var1 <= var2)
-		if(var1 < 0) var1 = -var1;
-		if(var2 < 0) var2 = -var2;
-		if(var2 < var1) {
-			int aux = var1;
-			var1 = var2;
-			var2 = aux;
-		}
-		
-	
-		//Rellenamos todos los descendientes
-		for(int i = 0; i < getReproductoresSize(); i++) {
-			this.addDescendiente(getReproductorAt(i));	
-		}
-				
-		//Intercambia las dos subcadenas entre [var1, var2]
-		for(int i = 0; i < getReproductoresSize(); i++) {
-			individuo ind1 = getReproductorAt(i);
-			individuo ind2 = getReproductorAt(i + 1);
-			
-			for(int j = var1; j < var2 - var1; j++){
-				gen aux = ind1.getCromosomaAt(j);
-				ind1.setGen(j, ind2.getCromosomaAt(j));
-				ind2.setGen(j, aux);
-			}
-			
-			this.addDescendiente(ind1);
-			this.addDescendiente(ind2);
-		}
-	}	
 }
