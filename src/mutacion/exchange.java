de8ddf43b4cfa113cbfdcb46f396716b870a3f4b
Sergio <sramos02@ucm.es>
2020-03-20 10:58:42 +0100

    Intercambio arreglado
    Ya mura con probabilidad pCruce

--- src/mutacion/exchange.java
+++ src/mutacion/exchange.java
@@ -11,19 +11,22 @@ public class exchange extends mutacion{
 	public void mutar(poblacion poblacion, double probMutacion) {
 		
 		Random rand = new Random();
-
+		double prob = Math.random()%1;
+		
 		for(int i = 0; i < poblacion.getSize(); i++) {
-			int var1 = rand.nextInt()%poblacion.getIndividuo(i).getSizeCromosoma();
-			int var2 = rand.nextInt()%poblacion.getIndividuo(i).getSizeCromosoma();
-			
-			//Ajustamos los datos
-			while(var1 == var2) var2 = rand.nextInt()%poblacion.getIndividuo(i).getSizeCromosoma();
-			if(var1 < 0) var1 = -var1;
-			if(var2 < 0) var2 = -var2;
-			
-			gen aux = poblacion.getIndividuo(i).getCromosomaAt(var1);
-			poblacion.getIndividuo(i).setGen(var1, poblacion.getIndividuo(i).getCromosomaAt(var2));
-			poblacion.getIndividuo(i).setGen(var2, aux);
+			if(prob < probMutacion){
+				int var1 = rand.nextInt()%poblacion.getIndividuo(i).getSizeCromosoma();
+				int var2 = rand.nextInt()%poblacion.getIndividuo(i).getSizeCromosoma();
+				
+				//Ajustamos los datos
+				while(var1 == var2) var2 = rand.nextInt()%poblacion.getIndividuo(i).getSizeCromosoma();
+				if(var1 < 0) var1 = -var1;
+				if(var2 < 0) var2 = -var2;
+				
+				gen aux = poblacion.getIndividuo(i).getCromosomaAt(var1);
+				poblacion.getIndividuo(i).setGen(var1, poblacion.getIndividuo(i).getCromosomaAt(var2));
+				poblacion.getIndividuo(i).setGen(var2, aux);
+			}
 		}
 	}
 
