81148ef34f4498221fef755da894b0901a4cb9c9
Sergio <sramos02@ucm.es>
2020-03-28 13:00:35 +0100

    metPropio arreglado

--- src/mutacion/metPropio.java
+++ src/mutacion/metPropio.java
@@ -8,50 +8,47 @@ import poblacion.poblacion;
 
 public class metPropio extends mutacion{
 
-	private boolean [] bMut;
-
 	@Override
 	public void mutar(poblacion poblacion, double probMutacion) {
-		int var1, var2;
+
+		Random rand=new Random();
 		
 		for(int i = 0; i < poblacion.getSize(); i++) {
 			double valor = Math.random()%1;	
+			
 			if(valor < probMutacion) {
-				Random rand=new Random();
+				
+				individuo mutado = poblacion.getIndividuo(i);
 				int tamCromosoma = poblacion.getIndividuo(i).getSizeCromosoma();
 				
-				var1 = rand.nextInt(tamCromosoma);
-				var2 = rand.nextInt(tamCromosoma);
-				
-				bMut = new boolean[tamCromosoma];
-
-				
-				while(var1 == var2)
-					var2 = rand.nextInt(tamCromosoma);
-			
-				if(var1 > var2) {
-					int aux = var1;
-					var1 = var2;
-					var2 = aux;
+				//Tomamos las posiciones necesarias y aseguramos que sean diferentes
+				//nuevaPos = ((var1 / tamCromosoma) * 10) % tamCromosoma
+				int var1 = rand.nextInt(tamCromosoma);
+				int var2 = rand.nextInt(tamCromosoma);
+				int nuevaPos = (var1/tamCromosoma)*10;
+					
+				while(var1 == var2) {
+					var1 = rand.nextInt(tamCromosoma);
+					nuevaPos = (var1/tamCromosoma)*10;
 				}
 				
-				muta(poblacion.getIndividuo(i), var1, var2, tamCromosoma);
+				if(var1 > var2) {
+					int var3 = var1;
+					var1 = var2;
+					var2 = var3;
+				}
+			
+				//Intercambia los puntos entre sí
+				gen aux = mutado.getCromosomaAt(nuevaPos);
+				gen aux2 = mutado.getCromosomaAt(var2);
+				
+				mutado.setGen(nuevaPos, mutado.getCromosomaAt(var1));
+				mutado.setGen(var2, aux);
+				mutado.setGen(var1, aux2);
+				
+				poblacion.setIndividuoAt(i, mutado);
 			}
 		}
-	}
-
-	//Coge dos puntos, genera un tercero a partir de la siguiente operacion
-	//((var1 / tamCromosoma) * 10) % tamCromosoma
-	//Ahora intercambia los puntos
-	//No se pierden datos ya que si dos puntos coinciden se mantiene el valor en los auxialiares
-	private void muta(individuo individuo, int var1, int var2, int tamCromosoma) {
-		int nuevaPos = (var1/tamCromosoma)*10;
-		gen aux = individuo.getCromosomaAt(nuevaPos%tamCromosoma);
-		gen aux2 = individuo.getCromosomaAt(var2);
-		
-		individuo.setGen(nuevaPos%tamCromosoma, individuo.getCromosomaAt(var1));
-		individuo.setGen(var2, aux);
-		individuo.setGen(var1, aux2);
 	}
 
 }
